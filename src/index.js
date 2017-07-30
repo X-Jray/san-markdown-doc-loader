42b7a4b388350f8eae9a4fc1ea746bc1b21430c8
zhangxuanjian <zhangxuanjian@baidu.com>
2017-07-30 19:24:33 +0800

    add .cache dir

--- src/index.js
+++ src/index.js
@@ -21,7 +21,43 @@ module.exports = function (source) {
         this.options.sanMarkdown
     );
 
-    let code = markdown(options).render(source);
+    let md = markdown(options);
+
+    let renderer = md.renderer;
+    let renderAttrs = renderer.renderAttrs;
+
+    md.renderer.renderAttrs = function (token) {
+
+        if (token.nesting === -1) {
+            return '';
+        }
+
+        let attrs = token.attrs;
+
+        if (!attrs) {
+            attrs = token.attrs = [];
+        }
+
+        let hasClassName = false;
+        for (let i = 0, len = attrs.length; i < len; i++) {
+            if (attrs[i][0] === 'class') {
+                attrs[i][1] += ' md';
+                hasClassName = true;
+                break;
+            }
+        }
+
+
+        if (!hasClassName) {
+            attrs.push(['class', 'md']);
+        }
+
+        return renderAttrs.call(renderer, token);
+
+    };
+
+    let code = md.render(source);
+
 
     parser.parse(code).then(
         ast => {
--- src/markdown.js
+++ src/markdown.js
@@ -54,39 +54,6 @@ module.exports = function (options) {
 
     let md = markdown(preset, options);
 
-    let renderer = md.renderer;
-    let renderAttrs = renderer.renderAttrs;
-
-    md.renderer.renderAttrs = function (token) {
-
-        if (token.nesting === -1) {
-            return '';
-        }
-
-        let attrs = token.attrs;
-
-        if (!attrs) {
-            attrs = token.attrs = [];
-        }
-
-        let hasClassName = false;
-        for (let i = 0, len = attrs.length; i < len; i++) {
-            if (attrs[i][0] === 'class') {
-                attrs[i][1] += ' md';
-                hasClassName = true;
-                break;
-            }
-        }
-
-
-        if (!hasClassName) {
-            attrs.push(['class', 'md']);
-        }
-
-        return renderAttrs.call(renderer, token);
-
-    };
-
     if (plugins && plugins.length) {
         plugins.forEach(plugin => {
             if (Array.isArray(plugin)) {
